11b5e9b041e277757d6b6d2e31069da63622b148
Andrey Shevtsov <dev-shevtsov@pm.me>
2022-02-03 00:01:38 +0600

    Started telegram_client class

---
+++ src/Client/telegram_client.py
@@ -0,0 +1,19 @@
+import http
+
+
+class TelegramClientMeta(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+
+        if cls not in cls._instance:
+            instance = super().__call__(*args, **kwargs)
+            cls._instance[cls] = instance
+        return cls._instance[cls]
+
+
+class TelegramClient(metaclass=TelegramClientMeta):
+    def f(self):
+        print("test")
+
+    f()
