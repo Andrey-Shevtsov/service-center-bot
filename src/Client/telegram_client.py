b63ee2915290c423ecd4e78426a5f6ad99c9846f
Andrey Shevtsov <dev-shevtsov@pm.me>
2022-02-05 20:06:56 +0600

    created initial empty component-files

--- src/Client/telegram_client.py
+++ src/Client/telegram_client.py
@@ -6,7 +6,6 @@ class TelegramClientMeta(type):
     _instance = {}
 
     def __call__(cls, *args, **kwargs):
-
         if cls not in cls._instance:
             instance = super().__call__(*args, **kwargs)
             cls._instance[cls] = instance
@@ -17,15 +16,21 @@ class TelegramClient(metaclass=TelegramClientMeta):
     _telegramId = ""
     _req_address = "https://api.telegram.org/"
 
+    def __init__(self):
+        pass
+
     def get_token(self):
         cfg = open('../cfgs/package.json')
         token = json.load(cfg)["bot-token"]
         self._req_address += token + "/"
 
-
     def send_message(self, msg):
         api_string = self._req_address + "sendMessage"
+        pass
 
         # need to get receiver's id
         # there is a problem with replying on messages
         # add start() method (put in await state)
+
+    def long_poll(self):
+        pass
